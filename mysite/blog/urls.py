57e3a9d0d515f15502c49a14f5052a8608c345f6
jimin son <40316562+jeff125@users.noreply.github.com>
2018-12-17 18:47:58 +0900

    Update urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.hola, name='hola'),
     path('link/', views.link, name='link'),
     path('trans/', views.trans, name='trans')
 ]
